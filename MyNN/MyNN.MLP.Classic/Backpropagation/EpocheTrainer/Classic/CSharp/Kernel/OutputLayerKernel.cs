e0cd1e2a5e270faed0b0a19c5141ab8916faee66
lsoft <lsoft@list.ru>
2014-12-05 22:02:26 +0500

    * fixed bugs + buggy implementation of c# backpropagation

---
+++ MyNN/MyNN.MLP.Classic/Backpropagation/EpocheTrainer/Classic/CSharp/Kernel/OutputLayerKernel.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using MyNN.MLP.LearningConfig;
+using MyNN.MLP.Structure;
+using MyNN.MLP.Structure.Layer;
+
+namespace MyNN.MLP.Classic.Backpropagation.EpocheTrainer.Classic.CSharp.Kernel
+{
+    internal class OutputLayerKernel
+    {
+        private readonly ILayer _layer;
+        private readonly ILearningAlgorithmConfig _config;
+
+        public OutputLayerKernel(
+            ILayer layer,
+            ILearningAlgorithmConfig config
+            )
+        {
+            if (layer == null)
+            {
+                throw new ArgumentNullException("layer");
+            }
+            if (config == null)
+            {
+                throw new ArgumentNullException("config");
+            }
+
+            _layer = layer;
+            _config = config;
+        }
+
+        public void CalculateOverwrite(
+            float[] currentLayerNET,
+
+            float[] previousLayerLastState,
+            float[] currentLayerLastState,
+            float[] currentLayerDeDz,
+
+            float[] desiredOutput,
+
+            float[] currentLayerWeights,
+            
+            float[] nabla,
+
+            int previousLayerNeuronCountTotal,
+            int currentLayerNeuronCount,
+
+            float learningRate,
+            float regularizationFactor,
+            float dataCount
+            )
+        {
+            for (var neuronIndex = 0; neuronIndex < currentLayerNeuronCount ; neuronIndex++)
+            {
+                float nOut = currentLayerNET[neuronIndex];
+                float deri = _layer.LayerActivationFunction.ComputeFirstDerivative(nOut);
+
+                float metric = _config.TargetMetrics.CalculatePartialDerivativeByV2Index(
+                    currentLayerLastState,
+                    desiredOutput,
+                    neuronIndex
+                    );
+
+                float n = deri*metric;
+
+                currentLayerDeDz[neuronIndex] = n;
+
+                int nablaNeuronShift = ComputeWeightIndex(previousLayerNeuronCountTotal, neuronIndex);
+
+                for (
+                    int weightIndex = 0; 
+                    weightIndex < previousLayerNeuronCountTotal; 
+                    ++weightIndex)
+                {
+                    float deltaWeight =
+                        learningRate *
+                        n *
+                        (previousLayerLastState[weightIndex] + regularizationFactor * currentLayerWeights[nablaNeuronShift + weightIndex] / dataCount);
+
+                    nabla[nablaNeuronShift + weightIndex] = deltaWeight;
+                }
+            }
+        }
+
+        public void CalculateIncrement(
+            float[] currentLayerNET,
+
+            float[] previousLayerLastState,
+            float[] currentLayerLastState,
+            float[] currentLayerDeDz,
+
+            float[] desiredOutput,
+
+            float[] currentLayerWeights,
+
+            float[] nabla,
+
+            int previousLayerNeuronCountTotal,
+            int currentLayerNeuronCount,
+
+            float learningRate,
+            float regularizationFactor,
+            float dataCount
+            )
+        {
+            for (var neuronIndex = 0; neuronIndex < currentLayerNeuronCount; neuronIndex++)
+            {
+                float nOut = currentLayerNET[neuronIndex];
+                float deri = _layer.LayerActivationFunction.ComputeFirstDerivative(nOut);
+
+                float metric = _config.TargetMetrics.CalculatePartialDerivativeByV2Index(
+                    currentLayerLastState,
+                    desiredOutput,
+                    neuronIndex
+                    );
+
+                float n = deri * metric;
+
+                currentLayerDeDz[neuronIndex] = n;
+
+                int nablaNeuronShift = ComputeWeightIndex(previousLayerNeuronCountTotal, neuronIndex);
+
+                for (
+                    int weightIndex = 0;
+                    weightIndex < previousLayerNeuronCountTotal;
+                    ++weightIndex)
+                {
+                    float deltaWeight =
+                        learningRate *
+                        n *
+                        (previousLayerLastState[weightIndex] + regularizationFactor * currentLayerWeights[nablaNeuronShift + weightIndex] / dataCount);
+
+                    nabla[nablaNeuronShift + weightIndex] += deltaWeight;
+                }
+            }
+        }
+
+        private static int ComputeWeightIndex(
+            int previousLayerNeuronCount,
+            int neuronIndex)
+        {
+            return
+                previousLayerNeuronCount * neuronIndex;
+        }
+
+
+    }
+}
---
+++ MyNN/MyNN.MLP.Classic/Backpropagation/EpocheTrainer/Classic/CSharp/Kernel/UpdateWeightKernel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNN.MLP.Classic.Backpropagation.EpocheTrainer.Classic.CSharp.Kernel
+{
+    internal class UpdateWeightKernel
+    {
+        public void UpdateWeigths(
+            float[] currentLayerWeights,
+            float[] nabla,
+            float batchSize
+            )
+        {
+            if (currentLayerWeights == null)
+            {
+                throw new ArgumentNullException("currentLayerWeights");
+            }
+            if (nabla == null)
+            {
+                throw new ArgumentNullException("nabla");
+            }
+
+            for (var cc = 0; cc < currentLayerWeights.Length; cc++)
+            {
+                currentLayerWeights[cc] += nabla[cc] / batchSize;
+            }
+        }
+    }
+}
